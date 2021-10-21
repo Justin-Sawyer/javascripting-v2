c2e5ac0e55f064f2bc18b16adf9e70adb247b8b7
Justin-Sawyer <justin.sawyer@icloud.com>
2021-10-21 19:00:22 +0000

    accordion navigation in place

--- home/views.py
+++ home/views.py
@@ -5,7 +5,8 @@ def index(request):
     """ A view to return the index page """
     context = {
         'title': 'JavaScripting : The Home Page.',
-        'pagename': 'Home.'
+        'pagename': 'Home.',
+        'script': f'https://www.yourjs.com/console/?code=console.log%28%27This+is+a+console+payground.+Enter+your+code+here%21%27%29%3B&dontRunLastBreak=1'
     }
     return render(request, 'home/index.html', context)
 
