0a724118a29cbd2eea2b79659e5bb07e36406045
Justin-Sawyer <justin.sawyer@icloud.com>
2021-10-21 10:51:18 +0000

    antypewriter imation now handled by y JS, footer in place

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('index2', views.index2, name='second home'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    context = {
+        'title': 'JavaScripting : The Home Page.',
+        'pagename': 'Home.'
+    }
+    return render(request, 'home/index.html', context)
+
+
+def index2(request):
+    """ A view to return the index page """
+    context = {
+        'title': 'JavaScripting : The Second Home Page.',
+        'section': 'Section',
+        'pagename': 'Second Home.'
+    }
+    return render(request, 'home/index2.html', context)
+
